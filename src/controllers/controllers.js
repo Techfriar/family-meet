c8881dc1ca0ba6101b708fceea7996acac5716db
abhy-techfriar <abhiraj@techfriar.com>
2024-07-12 11:38:17 +0530

    font

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,16 +1,11 @@
-// src/controllers/controllers.js
 
 export const getHome = (req, res) => {
-    res.render('index', { title: 'Home' }); // Render the EJS template with a title
-  };
-  
-  export const getAbout = (req, res) => {
-    res.render('about', { title: 'About Us' }); // Render another EJS template
-  };
-  
-  export const postFormData = (req, res) => {
-    const formData = req.body;
-    // Handle form data, e.g., save to database or process it
-    res.send(`Form data received: ${JSON.stringify(formData)}`);
-  };
-  
+  res.render('index', { title: 'Home' });
+};
+
+export const postFormData = (req, res) => {
+  const formData = req.body;
+  console.log(req.body);
+  // Handle form data, e.g., save to database or process it
+  res.send(`Form data received: ${JSON.stringify(formData)}`);
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,28 @@
 import express from 'express';
-import { getHome, getAbout, postFormData } from '../controllers/controllers.js';
-
+import { getHome, postFormData } from '../controllers/controllers.js';
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../../public/uploads')); 
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+
+const upload = multer({ storage: storage });
 
 // Define routes and map them to controller functions
 router.get('/', getHome); // Home route
-router.get('/about', getAbout); // About route
-router.post('/submit-form', postFormData); // Example form submission route
+router.post('/submit-form', upload.single('picture'), postFormData);
 
 export default router;
